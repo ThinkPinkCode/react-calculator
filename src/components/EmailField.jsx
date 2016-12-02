43a58924841d05607f8700a5c53395ea86126239
Melanie Myers <melanie.myers@365smartshop.com>
2016-12-02 16:20:53 -0500

    LeadCapture started

---
+++ src/components/EmailField.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+
+var EmailField = React.createClass({
+    onChange: function(e){
+
+    },
+
+    render: function() {
+        return(
+            <div className="form-group">
+                <input className="form-control" onChange={this.onChange} placeholder="Email"/>
+            </div>
+
+        );
+    }
+});
+
+module.exports = EmailField;
