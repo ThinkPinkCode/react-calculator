7ff108aecf4384ba2683d46ca3d7ce255d743aac
Melanie Myers <melanie.myers@365smartshop.com>
2016-12-05 09:58:32 -0500

    added name field

--- src/components/LeadCapture.jsx
+++ src/components/LeadCapture.jsx
@@ -1,11 +1,33 @@
 var React = require('react');
+var EmailField = require('./EmailField.jsx');
+var NameField = require('./NameFields.jsx');
+
 
 var LeadCapture = React.createClass({
+
+    onSubmit: function(e) {
+        if (!this.refs.fieldEmail.state.valid){
+            alert("Problem with email");
+        }
+        else {
+            //send request to email host or server
+            var httpRequestBody = {
+                email: this.refs.fieldEmail.state.value,
+                firstName: this.refs.fieldName.state.value,
+            };
+        }
+
+    },
+
+
     render: function() {
         return(
             <div className="col-sm-3">
                 <div className="panel panel-default">
                     <div className="panel panel-body">
+                        <NameField type="First" ref="fieldName"/>
+                        <EmailField ref="fieldEmail"/>
+                        <button className="btn btn-primary" onClick={this.onSubmit}>Submit</button>
 
                     </div>
                 </div>
